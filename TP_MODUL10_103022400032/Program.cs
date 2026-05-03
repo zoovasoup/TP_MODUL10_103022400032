c2c0d3dc2e3072374d084a142668414924fcdcad
zoovasoup <zoovasoup@proton.me>
2026-05-03 22:46:25 +0700

    add: swagger

--- TP_MODUL10_103022400032/Program.cs
+++ TP_MODUL10_103022400032/Program.cs
@@ -6,12 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
